79eec49558aaa05bc099dc4529065422916aa2a0
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2023-12-25 14:33:01 +0530

    Resolving error issues

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,12 +2,13 @@ package com.driver;
 
 public class Main {
     public static class A {
-        public String meth(String str) {
+        public String meth() {
           return "Invoking method from class A";
         }
     }
 
     public static class B extends A {
+        @Override
         public String meth() {
          return "Method is overridden in Extendend class B";
         }
