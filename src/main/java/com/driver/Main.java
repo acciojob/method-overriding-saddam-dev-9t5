48ff972fc48019b6be63ad7f01dfd22ade6fc09e
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2023-12-25 13:14:57 +0530

    commit by Saddam

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A {
+      public String meth() {
+          return "My name is Saddam";
+      }
+  }
+
+    public static class B extends A {
+     public String meth() {
+         return "Method is overridden in Extendend class B";
+     }
+  }
+
+  public static void main(String[] args) {
+      B objB = new B();
+      String res = objB.meth();
+        System.out.println(res);
+  }
 }
